fa04178c4908a0542e675ad3229fd2213a3e2031
ddzbxh <duanshuxunhuan@outlook.com>
2021-05-31 10:22:29 +0800

    2021/5/31

--- Python/test.py
+++ Python/test.py
@@ -1,3 +1,5 @@
+from nbt import nbt
+
 def main():
     f = open("r.0.0.mca", "rb")
     i = 0
@@ -15,7 +17,21 @@ def main():
                 print(hex(ord(c)) + " ", end="")
     f.close()
 
+def testNbt():
+    nbtfile = nbt.NBTFile()
+    nbtfile.name = "mynbt.mca"
+    nbtfile.tags.append(nbt.TAG_Int(name="DataVersion", value=1343))
+    chunk0_0 = nbt.TAG_Compound(name="Chunk [0, 0]        in world at (0,0)")
+    chunk0_0.tags.append(nbt.TAG_Int(name="DataVersion", value=1343))
+    # chunk0_0.tags.append(nbt.TAG_Compound(name="ForgeDataVersion").tags.append(nbt.TAG_Int(name="minecraft", value=1343)))
+    level = nbt.TAG_Compound(name="Level")
+    entities = nbt.TAG_Compound()
+    level.tags.append(entities)
+    chunk0_0.tags.append(level)
+    nbtfile.tags.append(chunk0_0)
+    nbtfile.write_file("mynbt.mca")
 
 if __name__ == "__main__":
+    # testNbt()
     main()
 
